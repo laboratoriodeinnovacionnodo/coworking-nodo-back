5ed1aaaa69e1d6fea146fc89499b352de9e35973
Agustin <agustinjalilleon@gmail.com>
2026-03-14 19:12:24 -0300

    fix: add PORT fallback

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT;
   await app.listen(port);
 
   console.log(`🚀 Server running on http://localhost:${port}`);
